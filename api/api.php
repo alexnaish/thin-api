1144241ece28d4c4cb0b7712e1d20ed44754c6d3
Alex Naish <lexi.drage@gmail.com>
2015-01-26 21:52:33 +0000

    remove api folder

--- api/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-// Base Application Settings
-define('DS', DIRECTORY_SEPARATOR);
-define('ROOT', dirname(dirname(__FILE__)));
-
-require_once (ROOT . DS . 'api' . DS . 'lib' . DS . 'bootstrap.php');
-
-?>
