17475398d783d272df8f636bd5c160c20bcf9584
Alex Naish <lexi.drage@gmail.com>
2015-02-17 11:05:59 +0000

    Update HTTP Status response list

--- lib/class/api.class.php
+++ lib/class/api.class.php
@@ -146,12 +146,17 @@ abstract class API {
     private function _requestStatus($code) {
         $status = array(  
             200 => 'OK',
+            201 => 'Created',
             204 => 'No Content',
+            400 => 'Bad Request'
             401 => 'Unauthorized',
+            403 => 'Forbidden',
             404 => 'Not Found',   
-            406 => 'Not Acceptable',   
             405 => 'Method Not Allowed',
+            406 => 'Not Acceptable',
+            409 => 'Conflict',
             500 => 'Internal Server Error',
+            501 => 'Not Implemented'
         ); 
         return ($status[$code])?$status[$code]:$status[500]; 
     }
